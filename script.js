b8dc8dbb264fb80a03025237c88461878dde9ce7
harveedayondon <149028437+harveedayondon@users.noreply.github.com>
2023-11-20 09:13:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function playGame(playerChoice) {
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+   
+    let result;
+    if (playerChoice === computerChoice) {
+        result = "It's a tie!";
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result = "You win!";
+    } else {
+        result = "You lose!";
+    }
+
+   
+    document.getElementById("result").innerText = `You chose ${playerChoice}. Computer chose ${computerChoice}. ${result}`;
+}
